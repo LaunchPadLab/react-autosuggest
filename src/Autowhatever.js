8ff4a1b2d1d5c97a5d7d5b3342444d921314fd39
Danny Parnella <danparnella@gmail.com>
2020-10-21 14:04:54 -0500

    Add container props and additional aria attribute (#1)
    * add aria-controls to div container
    
    * allow for container props to be passed down to Autowhatever
    
    * swap aria-owns with aria-controls

--- src/Autowhatever.js
+++ src/Autowhatever.js
@@ -388,7 +388,7 @@ export default class Autowhatever extends Component {
     const containerProps = {
       role: 'combobox',
       'aria-haspopup': 'listbox',
-      'aria-owns': itemsContainerId,
+      'aria-controls': itemsContainerId,
       'aria-expanded': isOpen,
       ...theme(
         `react-autowhatever-${id}-container`,
